8d0cd6148db4882d0b644bc71704981334bb9052
RobinWadekar <robwadekar3011@gmail.com>
2024-01-06 21:23:13 +0530

    Completed Registration Page

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3001;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Get the current page URL
+//     var currentPage = window.location.pathname;
+
+//     // Remove 'active' class from all nav links
+//     document.querySelectorAll('.nav-link').forEach(function (link) {
+//         link.classList.remove('active');
+//     });
+
+//     // Set 'active' class for the current page link
+//     if (currentPage === '/') {
+//         document.getElementById('home-link').classList.add('active');
+//     } else if (currentPage === '/features') {
+//         document.getElementById('features-link').classList.add('active');
+//     } else if (currentPage === '/pricing') {
+//         document.getElementById('pricing-link').classList.add('active');
+//     }
+// });
+
+// const { window } = new JSDOM();
+// globalThis.window = window;
+// globalThis.document = window.document;
+
+// $(".nav-item").click(function(){
+//     let currentNavId = this.id;
+//     $(".nav-item").removeAttribute("aria-current='page'");
+//     $(`${currentNavId}`).addAttribute("aria-current='page'");
+// });
+
+// $(".nav-item").click(function () {
+//     $(".nav-item").removeClass("active");
+//     let currentClick = this.id;
+//     $(this).addClass("active");
+// });
+
+
+app.use(express.static(__dirname+"/../public/"));
+
+app.get("/", (req, res)=>{
+    res.render(__dirname+"/../public/Registration.ejs");
+});
+
+app.post("/submit", (req, res)=>{
+    // alert("Do you Want to Submit the Form?");
+    res.render(__dirname+"/../public/Registration.ejs");
+});
+
+app.listen(port, () => {
+    // console.log(__dirname);
+    console.log(`Server running on http://localhost:${port}`);
+});
